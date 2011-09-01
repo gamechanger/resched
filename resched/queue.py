a5b9a7105480bde53244430de3517f9f4315a09b
Kiril Savino <aurumaeus@gmail.com>
2011-09-01 15:13:40 -0400

    some basic cleanup tools

--- resched/queue.py
+++ resched/queue.py
@@ -14,6 +14,8 @@ class Queue(RedisBacked):
     >>> from base import ContentType
     >>> q = Queue(Redis('localhost'), 'stuff', ContentType.JSON)
     >>> q.clear()
+    >>> assert q.size() == 0
+    >>> assert q.number_in_progress() == 0
     >>> assert not q.peek()
     >>> dict_value = {'hello': 'world'}
     >>> string_value = '{"hello": "world"}'
@@ -30,52 +32,78 @@ class Queue(RedisBacked):
     >>> assert q.size() == 0
     >>> assert q.number_in_progress() == 0
     >>> assert q.number_active_workers() == 1
+    >>> q.reclaim_tasks()
     """
+
+    work_ttl_seconds = 60
 
     def __init__(self, redis_client, namespace, content_type, worker_id='global'):
         RedisBacked.__init__(self, redis_client, namespace, content_type)
         self.worker_id = worker_id
-        self.QUEUE_KEY = 'queue.{ns}'.format(ns=namespace)
-        self.WORKERS_KEY = 'queue.{ns}.workers'.format(ns=namespace)
-        self.WORKING_KEY = 'queue.{ns}.working.{wid}'.format(ns=namespace, wid=worker_id)
+        self.QUEUE_LIST_KEY = 'queue.{ns}'.format(ns=namespace)
+        self.WORKER_SET_KEY = 'queue.{ns}.workers'.format(ns=namespace)
+        self.WORKING_LIST_KEY = 'queue.{ns}.working.{wid}'.format(ns=namespace, wid=worker_id)
+        self.WORKING_ACTIVE_KEY = 'queue.{ns}.active.{wid}'.format(ns=namespace, wid=worker_id)
+
+    def _on_activity(self):
+        self.server.sadd(self.WORKER_SET_KEY, self.worker_id)
+        self.server.set(self.WORKING_ACTIVE_KEY, 'active')
+        self.server.expire(self.WORKING_ACTIVE_KEY, self.work_ttl_seconds)
 
     def clear(self):
         with self.server.pipeline() as pipe:
             pipe.multi()
-            pipe.delete(self.QUEUE_KEY)
-            pipe.delete(self.WORKING_KEY)
-            pipe.srem(self.WORKERS_KEY, self.worker_id)
+            pipe.delete(self.QUEUE_LIST_KEY)
+            pipe.delete(self.WORKING_LIST_KEY)
+            pipe.delete(self.WORKING_ACTIVE_KEY)
+            pipe.srem(self.WORKER_SET_KEY, self.worker_id)
             pipe.execute()
 
+    def reclaim_tasks(self):
+        for worker_id in self.server.smembers(self.WORKER_SET_KEY):
+            active_key = 'queue.{ns}.active.{wid}'.format(ns=self.namespace, wid=worker_id)
+            if self.server.get(active_key):
+                continue
+            working_key = 'queue.{ns}.working.{wid}'.format(ns=self.namespace, wid=worker_id)
+            while self.server.llen(working_key):
+                self.server.rpoplpush(working_key, self.QUEUE_LIST_KEY)
+
     def size(self):
-        return self.server.llen(self.QUEUE_KEY)
+        return self.server.llen(self.QUEUE_LIST_KEY)
 
     def number_in_progress(self):
-        return self.server.llen(self.WORKING_KEY)
+        return self.server.llen(self.WORKING_LIST_KEY)
 
     def number_active_workers(self):
-        return self.server.scard(self.WORKERS_KEY)
+        return self.server.scard(self.WORKER_SET_KEY)
 
     def push(self, value):
-        self.server.lpush(self.QUEUE_KEY, self.pack(value))
+        self._on_activity()
+        self.server.lpush(self.QUEUE_LIST_KEY, self.pack(value))
 
     def pop(self, destructively=False):
-        self.server.sadd(self.WORKERS_KEY, self.worker_id)
-        v = self.server.rpop(self.QUEUE_KEY) if destructively else self.server.rpoplpush(self.QUEUE_KEY, self.WORKING_KEY)
+        self._on_activity()
+        v = self.server.rpop(self.QUEUE_LIST_KEY) if destructively else self.server.rpoplpush(self.QUEUE_LIST_KEY, self.WORKING_LIST_KEY)
         return self.unpack(v)
 
     def peek(self):
-        return self.unpack(self.server.lindex(self.QUEUE_KEY, 0))
+        self._on_activity()
+        return self.unpack(self.server.lindex(self.QUEUE_LIST_KEY, 0))
 
     def complete(self, value):
-        self.server.lrem(self.WORKING_KEY, self.pack(value))
+        self._on_activity()
+        self.server.lrem(self.WORKING_LIST_KEY, self.pack(value))
+
+    def noop(self):
+        self._on_activity()
 
     def unpop(self, value):
+        self._on_activity()
         packed = self.pack(value)
         with self.server.pipeline() as pipe:
             pipe.multi()
-            pipe.zrem(self.WORKING_KEY, packed)
-            self.server.lpush(self.QUEUE_KEY, packed)
+            pipe.lrem(self.WORKING_LIST_KEY, packed)
+            self.server.lpush(self.QUEUE_LIST_KEY, packed)
             pipe.execute()
 
 
