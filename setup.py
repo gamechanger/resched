44f7cd934a19ecdf436b7d1a7412e40abb243a86
Kiril Savino <aurumaeus@gmail.com>
2011-09-01 15:48:39 -0400

    bump version as pypi test

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setuptools.setup(
     name="Resched",
-    version="0.0.1",
+    version="0.0.2",
     author="Kiril Savino",
     author_email="kiril@gamechanger.io",
     description="super simple Redis scheduling & queueing in Python",
